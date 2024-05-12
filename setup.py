a623335ccae46e892f6b19f825b962a06a02d0c6
SamAndTheSun <167593161+SamAndTheSun@users.noreply.github.com>
2024-05-12 03:40:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.16'
+VERSION = '0.0.17'
 DESCRIPTION = "Automate Stanford's GREAT browser"
 LONG_DESCRIPTION = "A Selenium implementation in Python for Stanford's GREAT browser, allowing for quick and easy genomic analysis."
 
